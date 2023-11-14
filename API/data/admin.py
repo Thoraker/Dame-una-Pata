3ae84828b7e551a0c82957ca1782b2298a6e2120
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-11-13 23:44:32 -0300

    Changing model from database

--- API/data/admin.py
+++ API/data/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from data.models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Pet)
+admin.site.register(Photo)
+admin.site.register(Post)
+admin.site.register(Address)
--- API/data/models.py
+++ API/data/models.py
@@ -1,58 +1,84 @@
 from django.db import models
-from data.extensions import get_uuid
 
 
 # Create your models here.
 class Address(models.Model):
-    id = models.AutoField(primary_key=True)
-    public_id = models.CharField(max_length=250, default=get_uuid)
-    street = models.CharField(max_length=250)
-    building_number = models.CharField(max_length=250)
-    department_number = models.CharField(max_length=250)
-    commune = models.CharField(max_length=250)
-    region = models.CharField(max_length=250)
-    has_backyard = models.BooleanField(default=False)
+    street = models.CharField(max_length=100)
+    building_number = models.IntegerField()
+    department_number = models.IntegerField()
+    commune = models.IntegerField()
+    region = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.street} {self.building_number} {self.department_number} {self.commune} {self.region}"
+
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Addresses"
 
 
 class Photo(models.Model):
-    id = models.AutoField(primary_key=True)
-    public_id = models.CharField(max_length=250, default=get_uuid)
-    url = models.CharField(max_length=250)
+    url = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.url}"
+
+    class Meta:
+        verbose_name = "Photo"
+        verbose_name_plural = "Photos"
 
 
 class Post(models.Model):
-    id = models.AutoField(primary_key=True)
-    message = models.CharField(max_length=512)
+    message = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    photos = models.ForeignKey(Photo, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.message}"
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
 
 
 class Pet(models.Model):
-    id = models.AutoField(primary_key=True)
-    public_id = models.CharField(max_length=250, default=get_uuid)
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=50)
+    specie = models.CharField(max_length=50)
     age = models.IntegerField()
-    specie = models.CharField(max_length=250)
-    size = models.CharField(max_length=250)
-    need_backyard = models.BooleanField(default=False)
-    for_adoption = models.BooleanField(default=True)
-    photos = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50)
+    for_adoption = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    photos = models.ForeignKey(Photo, on_delete=models.CASCADE, null=True)
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.specie} {self.size} {self.for_adoption}"
+
+    class Meta:
+        verbose_name = "Pet"
+        verbose_name_plural = "Pets"
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    public_id = models.CharField(max_length=250, default=get_uuid)
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=250)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    avatar = models.CharField(max_length=250)
-    pets = models.ManyToManyField(Pet, through="Owners_pets")
+    user_name = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    avatar = models.CharField(
+        max_length=150,
+        default="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200",
+        editable=True,
+    )
+    is_active = models.BooleanField(default=True)
     houses = models.ForeignKey(Address, on_delete=models.CASCADE)
-    posted_by = models.ForeignKey(Post, on_delete=models.CASCADE)
+    pets = models.ManyToManyField(Pet)
 
+    def __str__(self):
+        return f"{self.name} {self.last_name} {self.email} {self.phone_number}"
 
-class Owners_pets(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    adoption_date = models.DateField(auto_now=True)
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
